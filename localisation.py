8b4e364f431dac170a904666d6f5772f936e3e49
jahooo666 <okraskaj@ee.pw.edu.pl>
2015-11-21 00:43:26 +0100

    >changed encoding - project encoding now is set to UTF-8 >added beutifull and free data file where we have all required records like language, currency.

--- localisation.py
+++ localisation.py
@@ -8,36 +8,15 @@ data = json.loads(response.read())
 
 country = data['results'][0]['address_components'][6]['long_name']
 country_short = data['results'][0]['address_components'][6]['short_name']
-
-
-print country_short
-#print len(pycountry.countries)
-#print len(pycountry.languages)
 country_object = pycountry.countries.get(alpha2 = country_short)
-print(country_object.numeric)
-
-
-kraje = list(pycountry.countries)
-pp = pprint.PrettyPrinter(depth=1)
-#pp.pprint(kraje)
-cc={}
-for country in kraje:
-    cc[country.alpha2]=country.name
-
-#pp.pprint(cc)
-
-print cc['EN']
-#aragonese = pycountry.languages.get('an')
-#print aragonese.name
-try:
-    print "tutaj try"
-    #print country_object.numeric
-    #print country_object.alpha2
-    language = pycountry.languages.get(iso639_1_code = 'an')
-    #print language.alpha2
-    #print language.alpha2
-    print language.common_name
-except:
-    pass
 country_full_name = country_object.official_name
-#print country_full_name
+
+with open('countryInfo.txt') as infile:
+    for line in infile:
+        linel = line.split("\t")
+        if len(linel) > 12:
+            print linel
+
+
+
+
