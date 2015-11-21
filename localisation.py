230acbeb746d339c1c2709bb66a43ffe2b1c989e
jahooo666 <okraskaj@ee.pw.edu.pl>
2015-11-21 06:08:27 +0100

    >getting interesting fact about this localisation

--- localisation.py
+++ localisation.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import pprint, os, pycountry, urllib, json, babel
+import random
+
 __author__ = 'Jasiek'
 pp = pprint.PrettyPrinter(depth=6)
 
@@ -18,17 +20,46 @@ country_sought_name_iso3 = country_object.alpha3
 
 
 slowniki = []
+
+komunikaty = {
+    'country_name': "Witaj w ",
+    'currency': "Tutejsza waluta to ",
+    'population': "Tutejsza populacja to ok. ",
+    'area': "Obszar kraju w km kw. to ok.",
+    'capital': "Stolicą jest ",
+}
+
 with open('countryInfo.txt') as infile:
     for line in infile:
         linel = line.split("\t")
         if ((len(linel) > 14) and not (linel[0].startswith('#'))):
-            languages = linel[15].split(",")
+
             country_name_iso3 = linel[1]
+            capital = linel[5]
+            area = linel[6]
+            population = linel[7]
             currency_name = linel[11]
-            slownik = {'country_name': country_name_iso3,'language': languages, 'currency': currency_name}
+            languagesli = linel[15].split(",")
+            fist = languagesli[0].split("-")[0]
+            if fist != '':
+                #print fist
+                try:
+                    lang = pycountry.languages.get(iso639_1_code = fist.name)
+                except:
+                    lang = ''
+                    pass
+
+            slownik = {'country_name': country_name_iso3,'language': languagesli[0], 'langfull': lang, 'currency': currency_name,'population': population, 'area':area, 'capital':capital}
             #print country_name_iso3
             if country_name_iso3 == country_sought_name_iso3:
-                print u"Nazwa szukanego języka to: " + languages[0]
+                print u"Nazwa szukanego języka to: " + languagesli[0]
+                #ran=random.randint(0,5)
+                key = random.choice(slownik.keys())
+                while(key == 'language' or key == 'langfull'):
+                    key = random.choice(slownik.keys())
+
+                print komunikaty[key] + slownik[key]
+
 
             slowniki.append(slownik)
 #pp.pprint(slowniki)
