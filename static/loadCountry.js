752fecedcd3329a764abb501d602d43097b87615
Paweł <muchap1@ee.pw.edu.pl>
2015-11-21 05:23:32 +0100

    Dodanie pliku js, odpwiedzialnego za lokalizowanie uzytkownika

---
+++ static/loadCountry.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', getCountryName, false );
+var lat;
+var lon;
+
+function getCountryName(){
+    $.ajax( $SCRIPT_ROOT + "/" + "lat?=" + lat + "lon?=" + lon,{ // Nie wiem czy tak powinien wygladac ten url
+        success: function (responseText, statusText, jqXHR){
+            print(responseText);
+        }
+    });
+}
+
+function getLocalization() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        //x.innerHTML = "Brak obsługi geolokalizacji.";
+    }
+}
+
+function showPosition(position) {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+}
