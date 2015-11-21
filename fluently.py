7c010528a4bd93321cf1d9203c7d64242e47808d
Marcin <dlugopix@gmail.com>
2015-11-21 01:17:07 +0100

    added phrases priority incrementation

--- fluently.py
+++ fluently.py
@@ -2,19 +2,21 @@ import json
 import sqlite3
 from contextlib import closing
 import os
-
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 
 app_url = '/fluently'
 app = Flask(__name__)
 app.config.from_pyfile('fluently.ini', silent=False)
 
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
 
 @app.before_request
 def before_request():
     g.db = connect_db()
+
 
 @app.teardown_request
 def teardown_request(exception):
@@ -29,29 +31,38 @@ def get_db():
         db = g._database = connect_db()
     return db
 
+
 @app.route('/phrasebook')
 def phrasebook():
-    #tworzenie strony
-    con = get_db().execute('select * from phrases')
+    # tworzenie strony
+    cur = get_db().execute('SELECT * FROM phrases')
     phrases = []
-    for phrase in con:
-        print(phrase)
+    for phrase in cur:
         phrases.append(phrase)
-    return render_template('index.html', phrases = phrases)
+    print(phrases)
+    return render_template('index.html', phrases=phrases)
 
 
+@app.route('/count_phrase', methods=['POST'])
+def countPhrase():
+    cur = connect_db()
+    id = request.args['id']
+    print(id)
+    cur.execute("""
+    UPDATE phrases
+    SET  priority = priority + 1
+    WHERE id is
+    """ + str(id))
+    cur.commit()
+    return phrasebook()
 
-@app.route('/countPhrase', methods=['POST'])
-def get_count_phrase():
-    return connect_db().execute('select priority from phrases where id is 0')
 
-def write_most_popular_places( localization ):
+def write_most_popular_places(localization):
     print("dsaa")
-    #baza.write("localization ++")
+    # baza.write("localization ++")
 
-
-def increment_phrase( phrase_id ):
-    connect_db().execute('update phrases set priority = priority + 1 where id is %id') %phrase_id
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', 5000)
+
+    # connect_db().execute( "update phrases set english = 'hello', polish = 'czesc', category = 'basic' where id = 0" );
